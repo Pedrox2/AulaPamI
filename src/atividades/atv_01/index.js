d2a41e31c25cbcf44a2b4ae97a198be986ba7a38
Pedrox2 <phfernandesmartins669@gmail.com>
2024-10-29 15:14:56 -0300

    Initial commit

---
+++ src/atividades/atv_01/index.js
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+
+import styles from './styles';
+
+export default function Atividade1 () {
+    return(
+        <View style={styles.container}>
+                <Text style={styles.titulo}>Introdução React Native</Text>
+                <Text style={styles.texto}>Aula 1 de react native</Text>
+        </View>
+
+
+
+    );
+
+
+}
---
+++ src/atividades/atv_01/styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+import { RFValue} from 'react-native-responsive-fontsize';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#a8e1ff',
+        padding: 8,
+    },
+    titulo: {
+        //fontSize: 20,
+        fontSize: RFValue(20),
+        color: '#fafafa',
+        fontWeight: 'bold',
+        margintop: 16,
+    },
+    texto: {
+        fontSize: 20,
+
+    },
+});
+
+export default styles;
