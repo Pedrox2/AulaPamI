fbfb9bcaa31efc1bece23858c1e55a9577e1f446
Pedrox2 <phfernandesmartins669@gmail.com>
2024-11-05 15:22:38 -0300

    Aula 05-11

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Exemplo1 from './src/exemplos/ex_01';
 import Exemplo2 from './src/exemplos/ex_02';
 
 import Atividade1 from './src/atividades/atv_01';
+import Atividade2 from './src/atividades/atv_02'; 
 
 
 
@@ -12,7 +13,7 @@ import Atividade1 from './src/atividades/atv_01';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo2 />
+      <Atividade2 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/atividades/atv_02/index.js
@@ -0,0 +1,23 @@
+import { View, Text, Image } from 'react-native'; 
+
+import Mensagem from './mensagem';
+
+import logo from '../../../assets/002-1-react-native.png'; 
+
+import styles from './styles';
+
+export default function Atividade2 () { 
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>
+                Atividade 2
+            </Text>
+            <Image source={logo} style={styles.imagem} /> 
+
+            <Mensagem nome={'Ewerton'} idade={'22'} />
+            <Mensagem nome={'Werton'}  idade={'12'}/>
+            <Mensagem nome={'Erton'}   idade={'2'}/>
+        </View>
+    );
+}
